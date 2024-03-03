fc8a6044bf4d5ec1bb6746cf5a1f178511b07637
Jai Sawhney <jai.sawhney1@gmail.com>
2024-03-02 23:00:30 -0800

    Moved some files around

--- src/events/commands/user.ts
+++ src/events/commands/user.ts
@@ -1,4 +1,6 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction, resolveColor, ColorResolvable } from 'discord.js';
+import { Prisma } from '@prisma/client';
+
 import config from '../../config.js';
 import prisma from '../../lib/prisma.js';
 
@@ -12,42 +14,60 @@ const builder = new SlashCommandBuilder()
     );
 
 const execute = async (interaction: ChatInputCommandInteraction) => {
-    const user = interaction.options.getUser('user')!;
-    const guildMember = await interaction.guild!.members.fetch(user.id)!;
-    const userData = await prisma.user.findUnique({
-        where: {
-            id: user!.id,
-        },
-    });
-
-    interaction.reply({
-        embeds: [
-            {
-                title: `User info for ${user?.tag}:`,
-                color: resolveColor(config.messages.embeds.colors.main as ColorResolvable),
-                fields: [
-                    {
-                        name: 'User ID',
-                        value: user.id,
-                    },
-                    {
-                        name: 'Joined on',
-                        value: guildMember.joinedAt!.toLocaleDateString('en-US'),
-                        inline: true,
-                    },
-                    {
-                        name: '# of Messages',
-                        value: `${userData?.numMessages || 0}`,
-                        inline: true,
-                    },
-                ],
-                thumbnail: {
-                    url: user.displayAvatarURL(),
-                },
+    try {
+        const user = interaction.options.getUser('user')!;
+        const guildMember = await interaction.guild!.members.fetch(user.id)!;
+        const userData = await prisma.user.findUnique({
+            where: {
+                id: user!.id,
             },
-        ],
-        ephemeral: true,
-    });
+        });
+
+        interaction.reply({
+            embeds: [
+                {
+                    title: `User info for ${user?.tag}:`,
+                    color: resolveColor(config.messages.embeds.colors.main as ColorResolvable),
+                    fields: [
+                        {
+                            name: 'User ID',
+                            value: user.id,
+                        },
+                        {
+                            name: 'Joined on',
+                            value: guildMember.joinedAt!.toLocaleDateString('en-US'),
+                            inline: true,
+                        },
+                        {
+                            name: '# of Messages',
+                            value: `${userData?.numMessages || 0}`,
+                            inline: true,
+                        },
+                    ],
+                    thumbnail: {
+                        url: user.displayAvatarURL(),
+                    },
+                },
+            ],
+            ephemeral: true,
+        });
+    } catch (err) {
+        if (err instanceof Prisma.PrismaClientKnownRequestError) {
+            // https://www.prisma.io/docs/orm/reference/error-reference#error-codes
+            // ...
+        }
+
+        console.error(err);
+        interaction.reply({
+            embeds: [
+                {
+                    title: 'There was an error while running the command!',
+                    color: resolveColor(config.messages.embeds.colors.error as ColorResolvable),
+                },
+            ],
+            ephemeral: true,
+        });
+    }
 };
 
 export { builder, execute };
